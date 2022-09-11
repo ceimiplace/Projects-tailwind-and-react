bc1daf9df774df471e6831473ce90c03e07c883a
ceimiplace <azamfiredenis@yahoo.com>
2022-09-11 21:35:14 +0300

    styles searchBar width

--- search-input-and-fetch-images/src/components/SearchBar.js
+++ search-input-and-fetch-images/src/components/SearchBar.js
@@ -1,7 +1,17 @@
 import React from "react";
 class SearchBar extends React.Component {
   render() {
-    return <div>Search bar</div>;
+    return (
+      <div className="flex justify-center">
+        <form className="w-10/12 p-3 border-black-700  shadow m-4 max-w-5xl">
+          <label className="font-bold block mb-4">Image search</label>
+          <input
+            type="text"
+            className="block w-full border-slate-300 border-2 rounded"
+          />
+        </form>
+      </div>
+    );
   }
 }
 export default SearchBar;
