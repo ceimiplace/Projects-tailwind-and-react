7793fd36a0f3e93431308dcb19f7b374e2022d25
ceimiplace <azamfiredenis@yahoo.com>
2022-09-13 09:11:42 +0300

    added fetch request

--- translation-api/src/App.js
+++ translation-api/src/App.js
@@ -1,5 +1,25 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React from "react";
 
+class App extends React.Component {
+  state = { text: "", source: "", target: "" };
+  getTranslation() {
+    const options = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        "X-RapidAPI-Key": "42cea698e9mshf610c2802833d6ap1e9bbejsnf9c364e9f959",
+        "X-RapidAPI-Host": "google-translate78.p.rapidapi.com",
+      },
+      body: `{ text: ${this.state.text}, source: ${this.state.source}, target: ${this.state.target} }`,
+    };
+
+    fetch("https://google-translate78.p.rapidapi.com/translate", options)
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err));
+  }
+  render() {
+    return <div className="App"></div>;
+  }
+}
 export default App;
