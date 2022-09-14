ab848c68e6e90e6659605f683837097c215474d2
ceimiplace <azamfiredenis@yahoo.com>
2022-09-14 08:05:59 +0300

    added Accordion component
    Accordion component can render any info passed as props from App component , and styled with tailwind works

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,9 +1,12 @@
+import React from "react";
 import Accordion from "./components/Accordion";
 
+const items = [{title : "What is React?",content:"React is front-end javascript framework"},{title:"Why use React?",content:"React is a favorite JS library among engineers"},
+{title:"How do you use React?",content : "You use react by creating components"}]
 function App() {
   return (
     <div className="App">
-     <Accordion/>
+     <Accordion items={items}/>
     </div>
   );
 }
--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,5 +1,14 @@
 import React from "react"
-const Accordion  = () =>{
- return <div>hy accordion</div>
+import { useState } from "react"
+const Accordion  = ({items}) =>{
+const [activeIndex,setActiveIndex] = useState("null")
+function onActiveClick(index){
+    console.log("you clicked" , index)
+}
+ const list = items.map((item,index)=><div className="border-2 border-slate-200 p-6" key={item.title}>
+    <div onClick={()=>onActiveClick(index)} className="font-bold text-lg mb-2"><i className="fa-solid fa-arrow-down mr-2"></i>{item.title}</div>
+    <div>{item.content}</div>
+ </div>)
+ return <div className="max-w-xl">{list}</div>
 }
 export default Accordion;
