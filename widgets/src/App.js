5075e804777527b6f4f4918d5467287bb40402b8
ceimiplace <azamfiredenis@yahoo.com>
2022-09-14 18:00:12 +0300

    added useState hook on  search component
    added useState hook that  changes state depending on user search input on Search omponent

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
 const items = [{title : "What is React?",content:"React is front-end javascript framework"},{title:"Why use React?",content:"React is a favorite JS library among engineers"},
 {title:"How do you use React?",content : "You use react by creating components"}]
 function App() {
   return (
     <div className="App">
-     <Accordion items={items}/>
+     {/* <Accordion items={items}/> */}
+     <Search/>
     </div>
   );
 }
--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,3 +1,12 @@
+import { useState } from "react"
 export default function Search(){
-    return <div>Hello</div>
+    
+    const [term,setTerm] = useState("")
+    console.log(term)
+    return <div className="search-container p-4">
+        <form onSubmit={e=>e.preventDefault()} className="">
+            <label className="font-bold">Enter search term</label>
+            <input value={term} onChange={e=>setTerm(e.target.value)} className = "block border-2 w-full border-slate-400 p-1"type="text"/>
+        </form>
+    </div>
 }
