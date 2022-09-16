d06293ebd6de3ee03891f60ba24c8f6fdaa5caf4
ceimiplace <azamfiredenis@yahoo.com>
2022-09-16 10:53:19 +0300

    renders search results and removes html from http request

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -9,19 +9,25 @@ export default function Search(){
         async function getData(){
             const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&origin=*&format=json&srsearch=${term}`
             const response = await fetch(url)
-            const data = await response.json();                
+            const data = await response.json();
+            console.log(data)                
             setResults(data.query.search)
         }
       getData();
       
     },[term])
+    function removeSpan(text){        
+        text = text.replaceAll(`<span class="searchmatch">`, " ")
+        text = text.replaceAll(`</span>`, " ")         
+        return text;
+    }
     return <div className="search-container p-4">
         <form onSubmit={e=>e.preventDefault()} className="">
             <label className="font-bold">Enter search term</label>
             <input value={term} onChange={e=>setTerm(e.target.value)} className = "block border-2 w-full border-slate-400 p-1"type="text"/>
         </form>
-        <div>
-            {results.map(elem=><li key={elem.pageid}>{elem.title} {elem.snippet}</li>)}
+        <div className="list-none">
+            {results.map(elem=><div key={elem.pageid}><div className="font-bold">{elem.title}</div>  <div>{removeSpan(elem.snippet)}</div></div>)}
         </div>
     </div>
 }
