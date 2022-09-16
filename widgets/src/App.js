def3909d9559b19ef5bef62f0ab144dd4ae2b439
ceimiplace <azamfiredenis@yahoo.com>
2022-09-16 19:58:10 +0300

    Removes already selected items on dropdown

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -4,7 +4,7 @@ import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 const items = [{title : "What is React?",content:"React is front-end javascript framework"},{title:"Why use React?",content:"React is a favorite JS library among engineers"},
 {title:"How do you use React?",content : "You use react by creating components"}]
-const options = [{label:"The color Red" , value:"Red"},{label:"The color blue",value:"Blue"}]
+const options = [{label:"The color Red" , value:"Red"},{label:"The color Blue",value:"Blue"}]
 function App() {
   return (
     <div className="App">
--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -2,6 +2,7 @@ import { useState } from "react"
 export default function Dropdown({options}){
  const [status,setStatus] = useState("hidden")
  const [select,setSelect]= useState("Select a option")
+ 
  function showOrHide(){
     if(status == "hidden"){
         setStatus("block")
@@ -9,7 +10,11 @@ export default function Dropdown({options}){
         setStatus("hidden")
     }
  }
-    const renderedOptions=options.map(elem=><div onClick={()=>setSelect(elem.label)} key={elem.value}>{elem.label}</div>)
+    const renderedOptions=options.map(elem=>{ 
+        if(elem.label === select){return null}
+        else{ return <div onClick={()=>setSelect(elem.label)} key={elem.value}>{elem.label}</div>}} 
+    )
+    console.log(select)
     return <div onClick={showOrHide}><p>{select} <i className="fa-sharp fa-solid fa-caret-down"/></p>
     <div className={status}>{renderedOptions}</div>
     </div>
