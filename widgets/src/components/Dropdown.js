7ae3776c9ad1a0ed27eca6af80df40e3b2ea5a14
ceimiplace <azamfiredenis@yahoo.com>
2022-09-16 20:46:45 +0300

    added styling to component
    borders,background color on hover , max width on component

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -12,10 +12,10 @@ export default function Dropdown({options}){
  }
     const renderedOptions=options.map(elem=>{ 
         if(elem.label === select){return null}
-        else{ return <div onClick={()=>setSelect(elem.label)} key={elem.value}>{elem.label}</div>}} 
+        else{ return <div className="hover:bg-slate-300 my-2" onClick={()=>setSelect(elem.label)} key={elem.value}>{elem.label}</div>}} 
     )
     console.log(select)
-    return <div onClick={showOrHide}><p>{select} <i className="fa-sharp fa-solid fa-caret-down"/></p>
-    <div className={status}>{renderedOptions}</div>
+    return <div className="p-2 rounded border-2 max-w-xs border-slate-500  cursor-pointer" onClick={showOrHide}><p className="flex justify-between"><span>{select}</span> <i className="fa-sharp fa-solid fa-caret-down"/></p>
+    <div className={`${status}`}>{renderedOptions}</div>
     </div>
 }
