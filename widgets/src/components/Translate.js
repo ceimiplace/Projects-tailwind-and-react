f0ec27bcc3957b0d1166aa3dc9c9312681131ab6
ceimiplace <azamfiredenis@yahoo.com>
2022-09-17 21:17:40 +0300

    added timeout and clear Function on useEfect

--- widgets/src/components/Translate.js
+++ widgets/src/components/Translate.js
@@ -5,10 +5,11 @@ export default function Translate({languages}){
     const [textToBeTranslated, setTextToBeTranslated] = useState("Dummy text")
     const [translation,setTranslation] = useState("")
     const optionsToRender = languages.map(elem=><option key={elem.value}  value={elem.value}>{elem.name}</option>)    
-    
+    console.log(typeof languageSource)
     useEffect(()=>{
-        const token = "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM"
-        const options = {
+        if(languageSource === languageTarget){return;}
+        const token = "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM";
+            const options = {
             method:"POST",
             headers: {"Content-Type": "application/json",
                },
@@ -18,24 +19,27 @@ export default function Translate({languages}){
                 target: languageTarget,
                 format: "text",
               })
-        }
-        
+        }        
         async function handle(){
          let response = await fetch(`https://translation.googleapis.com/language/translate/v2?key=${token}`,options)
          let data = await response.json();
          console.log(data)     
            setTranslation(data.data.translations[0].translatedText)
         }
-        handle();
+        let timeId=setTimeout(handle,500)
+        return ()=>{
+            clearTimeout(timeId)
+        }
+
     },[languageSource,languageTarget,textToBeTranslated,])
    
-    return <div className="flex">
+    return <div className="flex p-4">
             <div>
                 <label htmlFor="languages">Select origin language:</label>
                 <select onChange={(event)=>setLanguage(event.target.value)} id="languages" >{optionsToRender}</select>
                 <textarea className="block" rows="6" cols="50" value={textToBeTranslated} onChange={(e)=>setTextToBeTranslated(e.target.value)}/>
             </div>
-            <div>
+            <div className="ml-4">
                 <label>Select language to translate to :</label>
                 <select onChange={(event)=>{setTarget(event.target.value)}}>{optionsToRender}</select>
                 <p>{translation}</p>
