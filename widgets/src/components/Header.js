2363a8efb269c2dac8b2e6b819ffea713f7492a5
ceimiplace <azamfiredenis@yahoo.com>
2022-09-19 20:36:51 +0300

    styled Header Component

--- widgets/src/components/Header.js
+++ widgets/src/components/Header.js
@@ -1,8 +1,8 @@
 import Link from "./Link";
 
-export default function Headers({ sendDown }) {
+export default function Headers() {
   return (
-    <div>
+    <div className="flex justify-evenly border-2 border-slate-400 py-4 mb-4">
       <Link href="/">Accordion</Link>
       <Link href="/search">Search</Link>
       <Link href="/dropdown">Dropdown</Link>
