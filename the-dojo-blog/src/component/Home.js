5cb25e9e6ae8e0a24603284b05b9b6afb1c0770d
ceimiplace <azamfiredenis@yahoo.com>
2022-09-20 15:11:21 +0300

    added hardcode list to check
    added State for blogList, added hardcoded list to check , added styling on list elements

--- the-dojo-blog/src/component/Home.js
+++ the-dojo-blog/src/component/Home.js
@@ -1,3 +1,34 @@
+import { useState } from "react";
 export default function Home() {
-  return <div>Hi there</div>;
+  const blogs = [
+    {
+      title: "How are we doing today?",
+      body: "How are people doing today?",
+      author: "Dennis D.",
+      id: 1,
+    },
+    {
+      title: "How are we doing yesterday?",
+      body: "How are people doing yesterday?",
+      author: "Bianca B.",
+      id: 2,
+    },
+  ];
+  const [message, setMessage] = useState("Click me");
+  const [blogList, setBlogList] = useState(blogs);
+
+  let listBlogs = blogList.map((elem) => (
+    <div className="hover:shadow-md mb-4" key={elem.id}>
+      <h1 className="text-xl font-medium text-rose-500">{elem.title}</h1>
+      <p>Written by {elem.author}</p>
+    </div>
+  ));
+  function handleClick() {
+    setMessage("Well Well hi there");
+  }
+  return (
+    <div>
+      <div className="blog-container">{listBlogs}</div>
+    </div>
+  );
 }
