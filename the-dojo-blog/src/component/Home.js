3e85d05ef8b49c7ecf602931ffe073131e9fa63a
ceimiplace <azamfiredenis@yahoo.com>
2022-09-21 15:23:48 +0300

    added error handling

--- the-dojo-blog/src/component/Home.js
+++ the-dojo-blog/src/component/Home.js
@@ -8,36 +8,42 @@ export default function Home() {
 
   useEffect(() => {
     async function getData() {
-      let responsePlaceholder = await fetch(
-        "https://jsonplaceholder.typicode.com/comments/?_limit=15"
-      );
-      let dataPlaceholder = await responsePlaceholder.json();
-      let newDataPlaceholder = dataPlaceholder.map((elem) => {
-        return {
-          body: elem.body,
-          id: elem.id,
-        };
-      });
+      try {
+        let responsePlaceholder = await fetch(
+          "https://jsonplaceholder.typicode.com/comments/?_limit=15"
+        );
+        let dataPlaceholder = await responsePlaceholder.json();
+        let newDataPlaceholder = dataPlaceholder.map((elem) => {
+          return {
+            body: elem.body,
+            id: elem.id,
+          };
+        });
 
-      let responseNames = await fetch("https://randomuser.me/api/?results=15");
-      let dataNames = await responseNames.json();
-      let newNames = dataNames.results.map((elem, index) => {
-        const { title, first, last } = elem.name;
-        const { large, medium, thumbnail } = elem.picture;
-        return {
-          title,
-          first,
-          last,
-          large,
-          medium,
-          thumbnail,
+        let responseNames = await fetch(
+          "https://randomuser.me/api/?results=15"
+        );
+        let dataNames = await responseNames.json();
+        let newNames = dataNames.results.map((elem, index) => {
+          const { title, first, last } = elem.name;
+          const { large, medium, thumbnail } = elem.picture;
+          return {
+            title,
+            first,
+            last,
+            large,
+            medium,
+            thumbnail,
 
-          ...newDataPlaceholder[index],
-        };
-      });
-      console.log(newNames);
-      setShowLoading(false);
-      setShowBlogs(newNames);
+            ...newDataPlaceholder[index],
+          };
+        });
+        console.log(newNames);
+        setShowLoading(false);
+        setShowBlogs(newNames);
+      } catch (err) {
+        console.log(err);
+      }
     }
 
     getData();
