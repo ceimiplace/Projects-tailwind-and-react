3bc992012946fb86ff384894657ccbebc95d33b0
ceimiplace <azamfiredenis@yahoo.com>
2022-09-22 09:48:23 +0300

    added Router,Route and Swich

--- the-dojo-blog/src/App.js
+++ the-dojo-blog/src/App.js
@@ -1,11 +1,18 @@
 import Navbar from "./component/Navbar";
 import Home from "./component/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
-    <div className="App mx-auto max-w-2xl ">
-      <Navbar />
-      <Home />
-    </div>
+    <Router>
+      <div className="App mx-auto max-w-2xl ">
+        <Navbar />
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
