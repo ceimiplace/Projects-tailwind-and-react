a17b8170efe9a591fad4633f195bc28f49ac06b2
ceimiplace <100032730+ceimiplace@users.noreply.github.com>
2022-09-25 20:35:39 +0300

    deleted unnecesary file

--- the-dojo-blog/src/App2/Loader.js
+++
@@ -1,8 +0,0 @@
-export default function Loader() {
-  return (
-    <>
-      <div className="border-4 border-slate-400 h-20 w-20 rounded-full border-t-rose-500 animate-spin"></div>
-      <div>Loading......</div>
-    </>
-  );
-}
