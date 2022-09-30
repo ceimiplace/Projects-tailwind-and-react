6831fe6c64240cfe9c28d398c356160ada16f722
ceimiplace <100032730+ceimiplace@users.noreply.github.com>
2022-09-30 23:13:26 +0300

    .

--- the-blog/src/BlogRenderer.js
+++ the-blog/src/BlogRenderer.js
@@ -1,15 +1,19 @@
 import BlogPreview from "./BlogPreview";
 export default function BlogRenderer({ blogsToDisplay, message }) {
-  let blogsList = blogsToDisplay.map((blog) => {
-    return (
-      <BlogPreview
-        id={blog.id}
-        title={blog.title}
-        name={blog.name}
-        picture={blog.picture}
-      />
-    );
-  });
+  let blogsList;
+  if (blogsToDisplay) {
+    blogsList = blogsToDisplay.map((blog) => {
+      return (
+        <BlogPreview
+          id={blog.id}
+          title={blog.title}
+          name={blog.name}
+          picture={blog.picture}
+        />
+      );
+    });
+  }
+
   return (
     <>
       <div className="text-2xl font-medium text-rose-500">{message}</div>
--- the-blog/src/CreateBlog.js
+++ the-blog/src/CreateBlog.js
@@ -8,7 +8,12 @@ export default function CreateBlog() {
   return (
     <div className="w-3/4 border-2 border-slate-400 mx-auto p-4">
       <h2 className="text-3xl font-medium text-center">Add new log</h2>
-      <form className="text-lg font-meidum ">
+      <form
+        className="text-lg font-meidum "
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
         <div>
           {" "}
           <label>Blog Title:</label>
@@ -16,11 +21,13 @@ export default function CreateBlog() {
             className="block border-2 border-400-slate w-full"
             type="text"
             required
+            onChange={(e) => setTitle(e.target.value)}
           />
         </div>
         <div>
           <label>Blog Text:</label>
           <textarea
+            onChange={(e) => setText(e.target.value)}
             className="block border-2 border-400-slate w-full h-40"
             required
           />
@@ -28,21 +35,31 @@ export default function CreateBlog() {
         <div>
           <label>Blog Author:</label>
           <input
+            onChange={(e) => setAuthor(e.target.value)}
             className="block border-2 border-400-slate w-full"
             type="text"
             required
           />
-          {/* <button onClick={()=>{
-            fetch("https://api.jsonbin.io/v3/b/63373e2660c92b271df634cb", {
-              method: "POST",
-              headers: {
-                "Conten-type": "application/json",
-                "X-Master-Key":
-                  "$2b$10$doR7B.7fbc0i1PieOgswOOl/ekMCLujXbfgUJ/G/AD.jscgMa5v2G",
-              },body: JSON.stringify({
-                title
-              }))
-          }}>Submit blog</button> */}
+          <button
+            onClick={() => {
+              fetch("https://api.jsonbin.io/v3/b/63373e2660c92b271df634cb", {
+                method: "Put",
+                headers: {
+                  "Content-Type": "application/json",
+                  "X-Master-Key":
+                    "$2b$10$doR7B.7fbc0i1PieOgswOOl/ekMCLujXbfgUJ/G/AD.jscgMa5v2G",
+                },
+                body: JSON.stringify({
+                  title,
+                  text,
+                  author,
+                  picture: `https://robohash.org/${author}.png`,
+                }),
+              }).then((resp) => console.log(resp));
+            }}
+          >
+            Submit blog
+          </button>
         </div>
       </form>
     </div>
